347ed6dc3ec9655a6163f740ed2f9958c615a89a
Akash <giyerakash@gmail.com>
2025-05-23 18:32:59 -0400

    removed camera 51

--- point_policy/robot_utils/franka/process_data_robot.py
+++ point_policy/robot_utils/franka/process_data_robot.py
@@ -38,8 +38,8 @@ SAVE_PATH = Path(DATA_DIR) / "processed_data"
 
 cam_indices = {
     1: "rgb",
-    2: "rgb",
-    51: "rgb",
+    2: "rgb"
+    #51: "rgb",
 }
 states_file_name = "states"
 cmd_states_file_name = "commanded_states"
